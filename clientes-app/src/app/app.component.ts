b7966b666898c764d27de18edb6af3c106593576
Jose Fernandes <jofer.br@gmail.com>
2022-08-26 11:24:21 -0300

    Integração do Angular com o Spring Boot

---
+++ clientes-app/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, AfterViewInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements AfterViewInit {
+  title = 'clientes-app';
+
+  ngAfterViewInit() {
+    window.addEventListener('DOMContentLoaded', event => {
+
+      // Toggle the side navigation
+      const sidebarToggle = document.body.querySelector('#sidebarToggle');
+      if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+             document.body.classList.toggle('sb-sidenav-toggled');
+        }
+        sidebarToggle.addEventListener('click', event => {
+          event.preventDefault();
+          document.body.classList.toggle('sb-sidenav-toggled');
+          //localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+      }
+
+    });
+  }
+}
---
+++ clientes-app/src/app/clientes.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Cliente} from './clientes/cliente';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientesService {
+
+  constructor( private http: HttpClient) { }
+
+  salvar( cliente: Cliente ): Observable<Cliente> {
+    return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente)
+  }
+
+  getCliente(): Cliente {
+    let cliente: Cliente = new Cliente();
+    cliente.nome = 'Jose Geraldo';
+    cliente.cpf = '12345678901'
+    return cliente;
+  }
+}
